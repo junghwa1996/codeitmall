c23932f66b49221c43aefc13481c8dac187a0f0c
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-20 10:59:16 +0900

    ✨ 검색했을때 Search 페이지로 이동
    - SearchForm 컴포넌트 생성
    - 검색어가 없을때는 home으로 이동
    - router.push 활용하여 이동하기

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
-import styles from '@/styles/Home.module.css';
-import Link from 'next/link';
+import styles from "@/styles/Home.module.css";
+import SearchForm from "@/components/SearchForm";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h1>Codeitmall</h1>
+      <SearchForm />
       <ul>
         <li>
-          <Link href="/products/1">첫 번째 상품</Link>
+          <Link href='/products/1'>첫 번째 상품</Link>
         </li>
         <li>
-          <Link href="/products/2">두 번째 상품</Link>
+          <Link href='/products/2'>두 번째 상품</Link>
         </li>
         <li>
-          <Link href="/products/3">세 번째 상품</Link>
+          <Link href='/products/3'>세 번째 상품</Link>
         </li>
         <li>
-          <Link href="https://codeit.kr">코드잇</Link>
+          <Link href='https://codeit.kr'>코드잇</Link>
         </li>
       </ul>
     </div>
