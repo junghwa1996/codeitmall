ae9517b45e496ba43fd809643bbf0896bfe556e5
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-21 11:41:52 +0900

    ✨ API 연동

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,3 +1,31 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from '@/lib/axios';
+
 export default function Product() {
-  return <div>Product 페이지</div>;
+  const [product, setProduct] = useState();
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  async function getProduct(targetId) {
+    const res = await axios.get(`/products/${targetId}`);
+    const nextProduct = res.data;
+    setProduct(nextProduct);
+  }
+
+  useEffect(() => {
+    if (!id) return;
+
+    getProduct(id);
+  }, [id]);
+
+  if (!product) return null;
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <img src={product.imgUrl} alt={product.name} />
+    </div>
+  );
 }
