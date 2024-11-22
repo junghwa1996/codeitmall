40abb5f67954800e453d30483fe6d80d8981d515
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-22 14:12:39 +0900

    ✨ 공통 컴포넌트 추가하기

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Container from '@/components/Container';
 import '@/styles/global.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Headers />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
--- pages/search.js
+++ pages/search.js
@@ -22,17 +22,13 @@ export default function Search() {
     getProducts(q);
   }, [q]);
 
-
   return (
     <div>
-      <Header />
-      <Container>
-        <SearchForm initialValue={q} />
-        <h2 className={styles.title}>
-          <span className={styles.keyword}>{q}</span> 검색 결과
-        </h2>
-        <ProductList className={styles.productList} products={products} />
-      </Container>
+      <SearchForm initialValue={q} />
+      <h2 className={styles.title}>
+        <span className={styles.keyword}>{q}</span> 검색 결과
+      </h2>
+      <ProductList className={styles.productList} products={products} />
     </div>
   );
 }
