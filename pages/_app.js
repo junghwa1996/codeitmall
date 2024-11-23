16e651af12eb160fdad80bab4422e1b0b51cdd3a
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-23 11:48:11 +0900

    ✨ next에서의 context
    - 추가 스타일링

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import Header from '@/components/Header';
 import Container from '@/components/Container';
 import '@/styles/global.css';
+import { ThemeProvider } from '@/lib/ThemeContext';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider>
       <Header />
       <Container>
         <Component {...pageProps} />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
