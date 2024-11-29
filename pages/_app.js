6ac27efb03e8f1ff7f48707093448522e16b4244
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-29 18:26:27 +0900

    ✨ next/font/google 사용
    - 해당 패키지 사용을 위해 next 버전 업그레이드

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,12 @@ import Header from '@/components/Header';
 import Container from '@/components/Container';
 import '@/styles/global.css';
 import { ThemeProvider } from '@/lib/ThemeContext';
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: [],
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,10 +18,12 @@ export default function App({ Component, pageProps }) {
         <link rel='icon' href='/favicon.png' />
       </Head>
       <ThemeProvider>
-        <Header />
-        <Container>
-          <Component {...pageProps} />
-        </Container>
+        <div className={notoSansKR.className}>
+          <Header />
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </div>
       </ThemeProvider>
     </>
   );
