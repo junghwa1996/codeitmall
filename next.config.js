456cd40f7e7292f57f7cf49fd7373aae1874931c
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-29 14:01:39 +0900

    🔨 next.config image 경로 디테일 추가

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'learn-codeit-kr-static.s3.ap-northeast-2.amazonaws.com',
         port: '',
-        pathname: '/codeitmall/*',
+        pathname: '/codeitmall/**',
       },
     ],
   },
