0de681cd8eb6c9a350613bab8ab232ee1661a88d
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-29 23:45:36 +0900

    📦 강의에 따른 패키징 업데이트

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   async redirects() {
     return [
       {
-        source: '/products/:id', // 리다이렉션의 시작 URL
-        destination: '/items/:id', // 리다이렉션의 대상 URL
-        permanent: true, // 영구(301) 리다이렉트
+        source: '/products/:id',
+        destination: '/items/:id',
+        permanent: true,
       },
-    ];
+    ]
   },
   images: {
     remotePatterns: [
@@ -21,6 +20,6 @@ const nextConfig = {
       },
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
